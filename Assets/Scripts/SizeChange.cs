16a9d2e9d3ce06224c90d714db1e59bb57d6fa93
Mutsumi Suganuma <suganuma-m@tama.ac.jp>
2024-06-14 00:41:55 +0900

    in progress

--- Assets/Scripts/SizeChange.cs
+++ Assets/Scripts/SizeChange.cs
@@ -7,26 +7,48 @@ public class SizeChange : MonoBehaviour
     
     public TGUnity TGC;
     public bool consF=false;
-    public float devBy = 10.0f;
+    private float devBy = 20;
     private Rigidbody rb;
 
+    private int shrinkRate = 60;
+    private int frameRate  = 60;
     private float att=0.0f;
     private float med=0.0f;
+
+    private float attNow= 0.0f;
+    private float medNow= 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Application.targetFrameRate = frameRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        att = (att+TGC.tgd.eSense.attention/devBy)/2.0f;
-        med = (med+TGC.tgd.eSense.meditation/devBy)/2.0f;
-        rb.transform.localScale = new Vector3(att, med, (att+med)/2.0f);
+        if( att != TGC.tgd.eSense.attention){
+            att = TGC.tgd.eSense.attention;
+        }
+        if( med != TGC.tgd.eSense.meditation){
+            med = TGC.tgd.eSense.meditation;
+        }
+
+        attNow = (attNow*(shrinkRate-1)+att)/shrinkRate;
+        medNow = (medNow*(shrinkRate-1)+med)/shrinkRate;
+
+        float attSize = attNow/devBy;
+        if( attSize < 1 ){
+            attSize = 1.0f;
+        }
+        float medSize = medNow/devBy;
+        if( medSize < 1 ){
+            medSize = 1.0f;
+        }
+        rb.transform.localScale = new Vector3(medSize, attSize, (attSize+medSize)/2.0f);
         if( consF ){
-            Debug.Log( att + " " + med);
+            Debug.Log( attNow +";" + att + " -- " + medNow + ";" +med);
         }
     }
 }
